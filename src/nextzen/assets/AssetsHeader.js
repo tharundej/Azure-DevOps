72d84ceeb1ac25d99652e99e5f8e3e3c50eafc63
anas-infobell <anas@infobellit.com>
2023-11-24 17:20:06 +0530

    asset model bo size

--- src/nextzen/assets/AssetsHeader.js
+++ src/nextzen/assets/AssetsHeader.js
@@ -34,7 +34,7 @@ const AssetsHead = ({ filterSearch, filterData }) => {
           open={showForm}
           onClose={handleClose}
           PaperProps={{
-            sx: { maxWidth: 1000 },
+            sx: { maxWidth: 1200 },
           }}
           className="custom-dialog"
         >
