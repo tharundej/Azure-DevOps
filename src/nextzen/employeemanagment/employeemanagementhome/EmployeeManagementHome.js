b7f32d5022f20a682a1449bbe6056236672439c7
ani0310 <anilraina0310@gmail.com>
2023-11-26 07:18:49 +0530

    role wise views done

--- src/nextzen/employeemanagment/employeemanagementhome/EmployeeManagementHome.js
+++ src/nextzen/employeemanagment/employeemanagementhome/EmployeeManagementHome.js
@@ -1,4 +1,4 @@
-import React,{useContext} from 'react'
+import React,{useContext, useEffect, useState} from 'react'
 
 import ReusableTabs from 'src/nextzen/tabs/ReusableTabs';
 
@@ -9,12 +9,48 @@ import SalaryStructure from '../salarystructure/SalaryStructure';
 import RoleAndResponsiblity from '../roleandresponsiblity/RoleAndResponsiblity';
 
 import UserContext from 'src/nextzen/context/user/UserConext';
+import { forEach } from 'lodash';
 
 
 function EmployeeManagementHome  () {
   const {user}=useContext(UserContext)
   console.log(user,'from contest')
-    const tabLabels = ['EmployeeTable', 'Statoury','Salary Structure',"Roles"];
+  const [tabLabels,setTabLabels]=useState([])
+   // const tabLabels = ['EmployeeTable', 'Statoury','Salary Structure',"Roles"];
+    const dataObj=[
+      {
+        id:'employeeTable',
+        label:'EmployeeTable'
+      },
+      {
+        id:'statutory',
+        label:'Statutory'
+      },
+      {
+        id:'salaryStructure',
+        label:'Salary Structure'
+      },
+
+    ]
+
+    useEffect(()=>{
+      var arr = [];
+
+      dataObj?.forEach((item) => {
+        const permission = user?.rolePermissions.employeeManagement;
+        //console.log( typeof permission?.mainHeading,  permission?.mainHeading)
+      if (permission && permission.hasOwnProperty('mainHeading') && permission.mainHeading && permission[item.id]) {
+        console.log(`User Permission for ${item?.key}:`, permission);
+        console.log(`mainHeading for ${item?.key}:`, permission.mainHeading);
+
+        arr.push(item.label);
+      }
+      });
+      arr.push("Roles")
+      setTabLabels(arr);
+    },[user])
+
+    
   const tabContents = [
     <div>
 
