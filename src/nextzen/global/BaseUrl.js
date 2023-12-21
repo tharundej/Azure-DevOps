a2764edcba573ec940cfd08908d78597f6645d23
Harshapriyaroyal <harshapriya054@gmail.com>
2023-12-21 18:23:11 +0530

    Baseurl

--- src/nextzen/global/BaseUrl.js
+++ src/nextzen/global/BaseUrl.js
@@ -3,9 +3,9 @@
 //  export const baseUrl=`https://mallard-blessed-lobster.ngrok-free.app/erp`
 
 // main server*
-// export const baseUrl=`http://192.168.1.199:3001/erp`;
+export const baseUrl=`http://192.168.1.199:3001/erp`;
 
-export const baseUrl=`http://192.168.1.5:3001/erp`
+// export const baseUrl=`http://192.168.1.5:3001/erp`
 
 
 
