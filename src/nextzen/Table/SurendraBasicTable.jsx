09bd9a61566a4fcc02c8d02b6ac435186369e6f0
surendra-malli <mallisurendra98@gmail.com>
2023-12-22 16:38:50 +0530

    before going to new repo

--- src/nextzen/Table/SurendraBasicTable.jsx
+++ src/nextzen/Table/SurendraBasicTable.jsx
@@ -763,7 +763,6 @@ const IconOpen =()=>{
           <TableContainer   component={Paper} sx={{position:"sticky",top: 0, overflow: "unset", padding:'0px !important' ,  width: '100%', height:300, maxHeight:300, }}>
 
             <TableSelectedAction
-
               dense={table.dense}
 
               numSelected={table?.selected?.length}
