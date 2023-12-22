3ff25c2fed4dc9c3a68a9c9b27e314c318e0e78c
benakshreyas <benakshreyas@gmail.com>
2023-12-22 15:09:50 +0530

    bug fixed v1

--- src/nextzen/configaration/taxSectionConfiguration/TAxSectionConfig.jsx
+++ src/nextzen/configaration/taxSectionConfiguration/TAxSectionConfig.jsx
@@ -69,6 +69,10 @@ const token  =  (user?.accessToken)?user?.accessToken:''
 
    
   ];
+  const [errors, setErrors] = useState({
+    taxScheme: '',
+    taxSection: '',
+  });
   const [count,setCount] = useState(0)
   const [editData, setEditData] = useState();
   const [showEdit, setShowEdit] = useState(false);
@@ -130,8 +134,33 @@ const token  =  (user?.accessToken)?user?.accessToken:''
     console.log('submitted data111', data);
 
   });
+ // Function to validate the form fields
+ const validateForm = () => {
+  const errors = {};
 
+  // Validate taxSection
+  if (!formData?.taxSection?.match(/^[a-zA-Z0-9()\-_*]+$/)) {
+    errors.taxSection = 'Invalid characters. Only alphanumeric, (), -, _, * are allowed.';
+  }
+
+  // Validate taxScheme
+  if (!formData?.taxScheme?.match(/^[a-zA-Z0-9()\-_*]+$/)) {
+    errors.taxScheme = 'Invalid characters. Only alphanumeric, (), -, _, * are allowed.';
+  }
+
+  return errors;
+};
   const updateDepartment = async (data) => {
+      // Validate the form fields
+      const validationErrors = validateForm();
+  
+      // Update the error state for each field individually
+      setErrors(validationErrors);
+    
+      // If there are validation errors, return without making the API call
+      if (Object.keys(validationErrors).length > 0) {
+        return;
+      }
     const payload = {
         companyId:cmpId,
        configId:valueSelected?.configId,
@@ -286,6 +315,8 @@ const  deleteFunction =(rowdata, event)=>{
                 }
                 variant="outlined"
                 fullWidth
+                helperText={errors.taxSection}  // Display error message
+                error={Boolean(errors.taxSection)}  // Add error style
               />
 
               <TextField
@@ -297,12 +328,15 @@ const  deleteFunction =(rowdata, event)=>{
                 }
                 variant="outlined"
                 fullWidth
+                helperText={errors.taxScheme}  // Display error message
+                error={Boolean(errors.taxScheme)}  // Add error style
               />
           
 
               <TextField
                 label="Limit "
                 name="taxLimit"
+                type='number'
                 value={valueSelected?.taxLimit || null}
                  onChange={(e, newValue) =>
                   handleSelectChange('taxLimit', newValue , e|| null)
