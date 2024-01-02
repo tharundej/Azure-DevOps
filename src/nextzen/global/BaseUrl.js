f416fec675e9a4a849be8662106ba198f0c02e82
anas-infobell <anas@infobellit.com>
2024-01-02 10:25:43 +0530

    link

--- src/nextzen/global/BaseUrl.js
+++ src/nextzen/global/BaseUrl.js
@@ -1,11 +1,11 @@
 // ngrok
     
 
-  export const baseUrl=`https://mallard-blessed-lobster.ngrok-free.app/erp`
+// export const baseUrl=`https://mallard-blessed-lobster.ngrok-free.app/erp`
 
 
 // main server*
-// export const baseUrl=`http://192.168.1.199:3001/erp`;
+ export const baseUrl=`http://192.168.1.199:3001/erp`;
 
 // export const baseUrl=`http://192.168.1.5:3001/erp`
 
